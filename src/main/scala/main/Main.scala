2ea6850690845e38413732202bebddda6a94e8e0
to-fer <aferrero384@gmail.com>
2014-03-15 04:53:00 -0700

    Reformatted block.

--- src/main/scala/main/Main.scala
+++ src/main/scala/main/Main.scala
@@ -23,36 +23,38 @@ object Main extends App {
   val watchList = parseAll(file, r).get
   r.close()
 
-  val watchFutures = watchList map { case (watchPath, moveList) => {
-    def performMove(ePath: Path) = {
-      moveList foreach {
-        case (moveParams, movePath) => {
-          if (moveParams contains (ePath.extension)) {
-            val mover = new FileMover(movePath)
-            ePath.downloadFinish.onSuccess {
-              case p => mover.move(p)
+  val watchFutures = watchList map {
+    case (watchPath, moveList) => {
+      def performMove(ePath: Path) = {
+        moveList foreach {
+          case (moveParams, movePath) => {
+            if (moveParams contains (ePath.extension)) {
+              val mover = new FileMover(movePath)
+              ePath.downloadFinish.onSuccess {
+                case p => mover.move(p)
+              }
             }
           }
         }
       }
-    }
 
-    // Perform initial move on startup.
-    val dirStream = Files newDirectoryStream watchPath
-    val dirIt = dirStream.iterator
-    while(dirIt.hasNext)
-      performMove(dirIt.next)
-    dirStream.close()
+      // Perform initial move on startup.
+      val dirStream = Files newDirectoryStream watchPath
+      val dirIt = dirStream.iterator
+      while(dirIt.hasNext)
+        performMove(dirIt.next)
+      dirStream.close()
 
-    future {
-      watch(watchPath) {
-        case Created(eventPath) => {
-          println(eventPath)
-          performMove(eventPath)
+      future {
+        watch(watchPath) {
+          case Created(eventPath) => {
+            println(eventPath)
+            performMove(eventPath)
+          }
         }
       }
     }
-  }}
+  }
 
   watchFutures foreach (Await.ready(_, Duration.Inf))
 }
