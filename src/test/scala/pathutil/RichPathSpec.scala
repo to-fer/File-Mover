eae42ba2a4e8eb80097fe9ae24bc076f038255b1
to-fer <tony.j.ferrero@gmail.com>
2014-03-26 18:37:14 -0700

    Added RichPath test suite.

---
+++ src/test/scala/pathutil/RichPathSpec.scala
@@ -0,0 +1,58 @@
+package pathutil
+
+import _root_.pathutil.RichPath.path2RichPath
+
+import org.specs2.mutable.Specification
+import java.nio.file.{Files, Paths}
+import dispatch._, Defaults._
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
+import java.util.concurrent.TimeUnit
+import java.io.File
+
+class RichPathSpec extends Specification {
+  "RichPath" should {
+
+    "extension" in {
+      val extention = "txt"
+      val extensionPath = Paths.get(s"test.$extention")
+      extensionPath.extension mustEqual extention
+    }
+
+    "digest" in {
+      val firstTestPath = Paths.get("test.txt")
+      if (!Files.exists(firstTestPath))
+        Files.createFile(firstTestPath)
+      val firstTestFileContents = "test"
+      Files.write(firstTestPath, firstTestFileContents.getBytes)
+
+      val secondTestPath = Paths.get("different-test.txt")
+      if (!Files.exists(secondTestPath))
+        Files.createFile(secondTestPath)
+      Files.write(secondTestPath, (firstTestFileContents + " I'm different!").getBytes)
+
+      val firstTestFileDigest = firstTestPath.digest
+      val secondTestFileDigest = secondTestPath.digest
+
+      Files delete firstTestPath
+      Files delete secondTestPath
+
+      firstTestFileDigest mustNotEqual secondTestFileDigest
+    }
+
+    "downloadFinish" in {
+      val googleFile = new File("google-file.html")
+      val googleUrl = url("http://www.google.com")
+      val googleHtml = Http(googleUrl > as.File(googleFile))
+      val waitDuration = Duration.create(10, TimeUnit.SECONDS)
+      val downloadFinishFuture = Future { googleFile.toPath.downloadFinish(waitDuration) }
+      Await.ready(downloadFinishFuture, waitDuration)
+
+      val fileExists = googleFile.exists
+      googleFile.delete() // Cleanup
+
+      fileExists mustEqual true
+    }
+
+  }
+}
