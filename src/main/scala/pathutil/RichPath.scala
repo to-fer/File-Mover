54c02c09c45466c21794353dc967d5d4beb43468
to-fer <tony.j.ferrero@gmail.com>
2014-03-26 18:32:54 -0700

    Renamed variables.

--- src/main/scala/pathutil/RichPath.scala
+++ src/main/scala/pathutil/RichPath.scala
@@ -16,25 +16,25 @@ object RichPath {
 class RichPath(delegatePath: Path) {
 
   def extension = {
-    val name = delegatePath.getFileName.toString
-    name.substring(name.indexOf(".") + 1)
+    val fileName = delegatePath.getFileName.toString
+    fileName.substring(fileName.indexOf(".") + 1)
   }
 
   def digest: List[Byte] = {
-    val digest = MessageDigest.getInstance("MD5")
+    val md5Digest = MessageDigest.getInstance("MD5")
     val inputStream = Files.newInputStream(delegatePath)
-    val digestStream = new DigestInputStream(inputStream, digest)
+    val digestStream = new DigestInputStream(inputStream, md5Digest)
 
     while(digestStream.read(new Array[Byte](1 << 16)) != -1){}
 
-    val md = digestStream.getMessageDigest()
+    val messageDigest = digestStream.getMessageDigest()
     digestStream.close()
-    md.digest.toList
+    messageDigest.digest.toList
   }
 
   def downloadFinish(timeout: Duration = Duration.create(15, TimeUnit.MINUTES)): Future[Path] = future {
 
-    val blockTime = Duration.create(50, TimeUnit.MILLISECONDS)
+    val blockDuration = Duration.create(50, TimeUnit.MILLISECONDS)
 
     def blockTillDownloadFinish(prevDigest: List[Byte], timeLeft: Duration): Unit = {
       if (timeLeft > Duration.Zero) {
@@ -42,14 +42,14 @@ class RichPath(delegatePath: Path) {
        * Sleep for a short time to make sure to give the file time to download a bit more before reading
        * from it again. Otherwise we might read so fast that it will appear to not be downloading when that isn't the case.
        */
-        Thread.sleep(blockTime.length) // DO NOT REMOVE THIS. I'M WARNING YOU!
+        Thread.sleep(blockDuration.length) // DO NOT REMOVE THIS. I'M WARNING YOU!
         val curDigest = digest
 
         /*
          * If the digests differ then the file is still downloading. If the file's size is 0 then it has not yet started to
          * download, so we must wait longer.
          */
-        if (!(curDigest sameElements prevDigest) || Files.size(delegatePath) == 0) blockTillDownloadFinish(curDigest, timeout - blockTime)
+        if (!(curDigest sameElements prevDigest) || Files.size(delegatePath) == 0) blockTillDownloadFinish(curDigest, timeout - blockDuration)
       }
       else throw new TimeoutException("File took too long to download.")
     }
