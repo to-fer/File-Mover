3eec2c71cfaf856d625aa71df50f1608399488fc
to-fer <tony.j.ferrero@gmail.com>
2014-03-26 18:20:16 -0700

    Added a timeout of 1 hour to download. Added download timeout handling.

--- src/main/scala/main/Main.scala
+++ src/main/scala/main/Main.scala
@@ -13,6 +13,8 @@ import config.ConfigFileParser._
 import pathutil.RichPath._
 import com.twitter.logging.Logger
 import com.twitter.logging.config.{FileHandlerConfig, LoggerConfig}
+import java.util.concurrent.TimeUnit
+import scala.util.{Failure, Success}
 
 object Main extends App {
 
@@ -42,14 +44,14 @@ object Main extends App {
               logger.info(s"About to move $eventPath. If you don't see a move-conformation message, then there was a " +
                            "problem, probably with detecting when the file to be moved finished downloading.")
 
-              eventPath.downloadFinish.onSuccess {
-                case p => {
+              eventPath.downloadFinish(Duration.create(1, TimeUnit.HOURS)).onComplete {
+                case Success(_) => {
                   logger.info(s"Moving $eventPath")
-
-                  val eventPathAfterMove = mover.move(p)
-
+                  val eventPathAfterMove = mover.move(eventPath)
                   logger.info(s"$eventPath moved to $eventPathAfterMove.")
                 }
+                case Failure(_) =>
+                  logger.error(s"$eventPath has not been moved. It took too long to download.")
               }
             }
           }
