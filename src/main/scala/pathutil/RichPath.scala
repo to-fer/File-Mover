6f515cb6012334d8a5923eb46486321d56d1e649
to-fer <tony.j.ferrero@gmail.com>
2014-03-26 18:19:30 -0700

    Changed name of delegate path. Added support for specifying a time after which a download will time out.

--- src/main/scala/pathutil/RichPath.scala
+++ src/main/scala/pathutil/RichPath.scala
@@ -4,21 +4,25 @@ import java.nio.file.{Files, Path}
 import java.security.{DigestInputStream, MessageDigest}
 import scala.concurrent.{Future, future, ExecutionContext}
 import ExecutionContext.Implicits.global
+import scala.concurrent.duration.Duration
+import java.util.concurrent.{TimeoutException, TimeUnit}
+import scala.util.parsing.combinator.Parsers
+import scala.util.Failure
 
 object RichPath {
   implicit def path2RichPath(p: Path) = new RichPath(p)
 }
 
-class RichPath(p: Path) {
+class RichPath(delegatePath: Path) {
 
   def extension = {
-    val name = p.getFileName.toString
+    val name = delegatePath.getFileName.toString
     name.substring(name.indexOf(".") + 1)
   }
 
   def digest: List[Byte] = {
     val digest = MessageDigest.getInstance("MD5")
-    val is = Files.newInputStream(p)
+    val is = Files.newInputStream(delegatePath)
     val digestStream = new DigestInputStream(is, digest)
 
     while(digestStream.read(new Array[Byte](1 << 16)) != -1){}
@@ -28,27 +32,30 @@ class RichPath(p: Path) {
     md.digest.toList
   }
 
-  def downloadFinish: Future[Path] = future {
+  def downloadFinish(timeout: Duration = Duration.create(15, TimeUnit.MINUTES)): Future[Path] = future {
 
-    def blockTillDownloadFinish(prevDigest: List[Byte]): Unit = {
+    val blockTime = Duration.create(50, TimeUnit.MILLISECONDS)
 
-      /*
+    def blockTillDownloadFinish(prevDigest: List[Byte], timeLeft: Duration): Unit = {
+      if (timeLeft > Duration.Zero) {
+        /*
        * Sleep for a short time to make sure to give the file time to download a bit more before reading
        * from it again. Otherwise we might read so fast that it will appear to not be downloading when that isn't the case.
        */
-      Thread.sleep(50) // DO NOT REMOVE THIS. I'M WARNING YOU!
-      val curDigest = digest
+        Thread.sleep(blockTime.length) // DO NOT REMOVE THIS. I'M WARNING YOU!
+        val curDigest = digest
 
-      /*
-       * If the digests differ then the file is still downloading. If the file's size is 0 then it has not yet started to
-       * download, so we must wait longer.
-       */
-      if (!(curDigest sameElements prevDigest) || Files.size(p) == 0) blockTillDownloadFinish(curDigest)
+        /*
+         * If the digests differ then the file is still downloading. If the file's size is 0 then it has not yet started to
+         * download, so we must wait longer.
+         */
+        if (!(curDigest sameElements prevDigest) || Files.size(delegatePath) == 0) blockTillDownloadFinish(curDigest, timeout - blockTime)
+      }
+      else throw new TimeoutException("File took too long to download.")
     }
 
-    blockTillDownloadFinish(List[Byte](0))
-
-    p
+    blockTillDownloadFinish(List[Byte](0), timeout)
+    delegatePath
   }
 
 }
