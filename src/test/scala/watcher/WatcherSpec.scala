0ff7416d2de2dbc2ae0a91d9961c1a31c45fcde8
to-fer <tony.j.ferrero@gmail.com>
2014-08-13 17:38:24 -0700

    Add WatcherSpec.

---
+++ src/test/scala/watcher/WatcherSpec.scala
@@ -0,0 +1,58 @@
+
+import java.nio.file.{Files, Path, Paths}
+
+import org.specs2.mutable._
+import watch.Watcher._
+
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, future}
+
+private class WatcherSpec extends Specification {
+
+  // Directory used for each test should be different, as these tests will be executed in parallel.
+  "Watcher" should {
+    "watch" in {
+      val (watchDir, watchDirContents) = prepareWatchDirectory("watcher_test_dir_2")
+
+      var eventPaths = List[Path]()
+
+      val watchFuture = future {
+        watch(watchDir, eventPaths.length == watchDirContents.length) {
+          case Created(p) => eventPaths = p :: eventPaths
+        }
+      }
+
+      // static watch method takes some time to start watching, so we have to wait a bit before making files.
+      Thread.sleep(100)
+      watchDirContents foreach { p => Files.createFile(p) }
+
+      // wait for the watch method to finish handling all events.
+      Await.ready(watchFuture, Duration(1000, "milliseconds"))
+
+      // cleanup
+      deleteWatchDirectory(watchDir, watchDirContents)
+
+      eventPaths.reverse mustEqual watchDirContents
+    }
+  }
+
+  def prepareWatchDirectory(watchDirStr: String): (Path, List[Path]) = {
+    val watchDir = Paths.get(watchDirStr)
+    val watchDirContents = List(
+      watchDir resolve "test1.txt",
+      watchDir resolve "test2.txt",
+      watchDir resolve "test3.txt",
+      watchDir resolve "test4.txt"
+    )
+
+    if (Files exists watchDir) deleteWatchDirectory(watchDir, watchDirContents)
+    Files.createDirectory(watchDir)
+
+    (watchDir, watchDirContents)
+  }
+
+  def deleteWatchDirectory(watchDir: Path, contents: List[Path]): Unit = {
+    contents foreach { Files.deleteIfExists }
+    Files.deleteIfExists(watchDir)
+  }
+}
