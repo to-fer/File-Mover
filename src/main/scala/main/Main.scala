effa8574c44474e071fcd8d3aa103fd1d9514047
to-fer <tony.j.ferrero@gmail.com>
2015-09-16 15:13:32 -0700

    Change how error handling is performed.
    Error handling was being done in a rather non-functional way, which is not desirable. The Try class is now used to encapsulate possible Throwables that may be thrown at certain points. All errors are handled at the end of the program, if any exist.

--- src/main/scala/main/Main.scala
+++ src/main/scala/main/Main.scala
@@ -1,52 +1,62 @@
 package main
 
-import java.io.FileReader
-import java.nio.file.{Files, Paths}
+import java.io.{FileNotFoundException, FileReader}
+import java.nio.file.{Path, Files, Paths}
 
 import _root_.path.FileMover
 import com.twitter.logging.Logger
 import com.twitter.logging.config.{FileHandlerConfig, LoggerConfig}
 import config.ConfigFileParser._
+import config.error.NoMoveRulesException
 import watch.Watcher._
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, future}
+import scala.util.{Try, Success, Failure}
 
 object Main extends App {
-
-  val osName = sys props "os.name"
-  val homeDir = sys env "HOME"
-  val configFilePath =
-    if (osName.contains("Linux"))
-      Paths.get(homeDir, ".config", "file_mover", "move")
-    else if (osName.contains("Windows"))
-      Paths.get(homeDir, ".file_mover", "move.txt")
+  def getConfigDirectoryPath(): Try[Path] = Try({
+    val home = sys.env("HOME")
+    val osName = sys.props("os.name")
+    if (osName.contains("Windows"))
+      Paths.get(home, ".file_mover")
     else
-      Paths.get("move.txt")
-  if (!Files.exists(configFilePath)) {
-    Files.createDirectories(configFilePath.getParent)
-    Files.createFile(configFilePath)
-  }
+      Paths.get(home, ".config", "file_mover")
+  })
 
-  // TODO detect when move rule conflict.
-  val configFileReader = new FileReader(configFilePath.toFile)
-  val watchList = parseAll(file, configFileReader).get
-  configFileReader.close()
-
-  val logFile = configFilePath.getParent resolve "mover_log.log"
-  val logger = Logger.get(getClass)
-  val config = new LoggerConfig {
-    handlers = new FileHandlerConfig {
-      filename = logFile.toString
+  def createFileIfNotExists(p: Path): Unit = {
+    if (!Files.exists(p)) {
+      Files.createDirectories(p.getParent)
+      Files.createFile(p)
     }
   }
-  config()
 
-  if (watchList.isEmpty)
-    logger.warning(s"There are no move rules specified in $configFilePath.")
+  val configDirectory = getConfigDirectoryPath()
+  val logFile = configDirectory.flatMap(p => Try(p.resolve("mover_log.log")))
+  logFile.foreach(createFileIfNotExists)
+  val logger = Logger.get(getClass)
+  val config = logFile.flatMap(log => Try(new LoggerConfig {
+    handlers = new FileHandlerConfig {
+      filename = log.toString
+    }
+  }))
+  config.foreach(_())
 
-  val watchFutures = watchList map {
+  val configFile = configDirectory.flatMap(p => Try(p.resolve("move.txt")))
+  configFile.foreach(createFileIfNotExists)
+
+  // TODO detect when move rules conflict.
+  val watchList = configFile.flatMap(p => Try({
+    val configFileReader = new FileReader(p.toFile)
+    val watches = parseAll(file, configFileReader).get
+    configFileReader.close()
+    if (watches.isEmpty)
+      throw new NoMoveRulesException(s"$p contains no move rules!")
+    watches
+  }))
+
+  val watchFutures = watchList.map(_.map {
     case (watchPath, moveList) => {
       val fileMovers = moveList map {
         case (moveParams, movePath) =>
@@ -73,7 +83,18 @@ object Main extends App {
         }
       }
     }
-  }
+  })
 
-  watchFutures foreach (Await.ready(_, Duration.Inf))
+  watchFutures match {
+    case Success(futures) => futures.foreach(Await.ready(_, Duration.Inf))
+    case Failure(exception) => exception match {
+      case ex: NoMoveRulesException => logger.error("Supplied move file contains no move rules.")
+      case ex: NoSuchElementException => logger.error("HOME environment variable does not exist; you must define it.")
+      case ex: FileNotFoundException => logger.error("Move file not found.")
+      case ex => {
+        logger.error("Unknown failure!")
+        ex.printStackTrace()
+      }
+    }
+  }
 }
